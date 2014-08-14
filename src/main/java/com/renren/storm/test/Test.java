6caf394a578848d054bc272f145803058060630c
Jicheng.Song <a@a.com>
2014-08-14 10:23:20 +0800

    modify Test.java

--- src/main/java/com/renren/storm/test/Test.java
+++ src/main/java/com/renren/storm/test/Test.java
@@ -3,6 +3,9 @@ package com.renren.storm.test;
 import backtype.storm.Config;
 import backtype.storm.LocalCluster;
 import backtype.storm.StormSubmitter;
+import backtype.storm.generated.AlreadyAliveException;
+import backtype.storm.generated.InvalidTopologyException;
+import backtype.storm.generated.StormTopology;
 import backtype.storm.testing.TestWordSpout;
 import backtype.storm.topology.TopologyBuilder;
 import backtype.storm.utils.Utils;
@@ -11,29 +14,36 @@ public class Test {
 
 	/**
 	 * @param args
+	 * @throws InvalidTopologyException 
+	 * @throws AlreadyAliveException 
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws AlreadyAliveException, InvalidTopologyException {
 		// TODO Auto-generated method stub
 
 		System.out.println("begin ");
 		TopologyBuilder builder = new TopologyBuilder(); 
 		//设置Spout
-		builder.setSpout("words", new TestWordSpout(), 2); 
+		builder.setSpout("spout_01", new TestWordSpout(), 1); 
 		//设置Bolt
-		builder.setBolt("exclaim1", new ExclamationBolt(), 3)
-		        .shuffleGrouping("words");
-		builder.setBolt("exclaim2", new ExclamationBolt(), 2)
-		        .shuffleGrouping("exclaim1");
+		builder.setBolt("bolt_01", new ExclamationBolt(), 1)
+		        .shuffleGrouping("spout_01");
+		builder.setBolt("bolt_02", new ExclamationBolt(), 1)
+		        .shuffleGrouping("bolt_01");
 		//
+		StormTopology topology = builder.createTopology();
 		Config conf = new Config();
-		conf.setDebug(true);
+		
+		conf.setDebug(false);
 		conf.setNumWorkers(2);
+		conf.setNumWorkers(8);
+		conf.setMaxSpoutPending(5000);
+		StormSubmitter.submitTopology("my_second_topology", conf, topology);
+		
+		
 		//StormSubmitter.submitTopology("asdfasdf", conf, builder.createTopology());
-		LocalCluster cluster = new LocalCluster();
-		cluster.submitTopology("test", conf, builder.createTopology());
-		Utils.sleep(10000);
-		cluster.killTopology("test");
-		cluster.shutdown();
+		Utils.sleep(1000);
+		// cluster.killTopology("test");
+		// cluster.shutdown();
 
 		System.out.println("end...");
 	}
